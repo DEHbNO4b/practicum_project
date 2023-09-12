277e1e2d5bf59a7771ba36989c1bcfde654dceba
Teymuraz <dehbno4b@gmail.com>
2023-09-12 19:29:58 +0300

    Revert "created log, config"
    This reverts commit 90435e6407f442fe8bc13b90c85d823dea2decc0.

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,26 +1,3 @@
 package main
 
-import (
-	"os"
-
-	"github.com/DEHbNO4b/practicum_project/internal/logger"
-	"github.com/DEHbNO4b/practicum_project/internal/maindb"
-)
-
-func main() {
-	if err := logger.Initialize("info"); err != nil {
-		panic(err)
-	}
-	if err := run(); err != nil {
-		logger.Log.Fatal(err.Error())
-		os.Exit(0)
-	}
-}
-func run() error {
-	cfg := parseFlag()
-	pdb, err := maindb.NewPostgresDB(cfg.Database_url)
-	if err != nil {
-
-	}
-	return nil
-}
+func main() {}
--- go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/DEHbNO4b/practicum_project
-
-go 1.20
-
-require (
-	go.uber.org/multierr v1.10.0 // indirect
-	go.uber.org/zap v1.25.0 // indirect
-)
